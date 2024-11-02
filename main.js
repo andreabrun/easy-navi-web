b78e0351fadd9bb61307dc39815a64fa03484b61
andreabrun <andrea.brun.tv@gmail.com>
2024-11-02 16:42:42 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,111 @@
+import "./assets/css/tailwind.css";
+
+let switchers = document.querySelectorAll(".switcher");
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+// EVENT LISTNER FOR THEME CHANGE
+switchers.forEach((switcher) => {
+  switcher.addEventListener("click", function () {
+    if (localStorage.getItem("color-theme")) {
+      if (localStorage.getItem("color-theme") === "light") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      }
+    } else {
+      if (document.documentElement.classList.contains("dark")) {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("color-theme", "light");
+      } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("color-theme", "dark");
+      }
+    }
+  });
+});
+
+// LANGUAGE TOGGLER
+function langToggle() {
+  let isItalian = document.documentElement.getAttribute('lang').includes('it');
+  let newLang = isItalian ? 'en' : 'it'
+  document.documentElement.setAttribute('lang', newLang);
+  document.querySelector('#lang-toggle').className = newLang;
+}
+
+
+// NAVBAR ACTIVE ELEMENT
+document.addEventListener('DOMContentLoaded', () => {
+  const observerOptions = {
+    root: null, // Use the viewport as the root
+    rootMargin: '-100px 0px -100px 0px', // Offset vertical by 100px
+    threshold: .75 // Trigger when 75% of the element is visible
+  };
+
+  const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const navItems = document.querySelectorAll('header a');
+        const currentNavItem = document.querySelector(`header a[href='#${entry.target.id}']`);
+        
+        if (currentNavItem) {
+          navItems.forEach(item => item.classList.remove('active'));
+          currentNavItem.classList.add('active');
+        }
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+  document.querySelectorAll('section[id]').forEach(element => { observer.observe(element) });
+});
+
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-cards'
+
+const screenSlides = new Swiper('.screenSlides', {
+    effect: "cube",
+    cubeEffect: {
+        slideShadows: false,
+        shadow: false,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    loop: true,
+    autoplay: {
+        delay: 1500,
+        duration: 500
+    },
+    grabCursor: true,
+    centeredSlides: true,
+    pagination: {
+        el: '.swiper-pagination',
+    }
+});
+
+// DOWNLOAD SECTION
+function showDownloadSection() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const downloadSection = document.getElementById('download');
+
+    if (downloadSection) {
+        downloadSection.classList.toggle('hidden', !urlParams.has('download'));
+    }
+}
+
+// CHECKS THE URL PARAMS AND SHOWS THE DOWNLOAD SECTION
+document.addEventListener('DOMContentLoaded', (event) => {
+    showDownloadSection();
+});
---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+import themer from "@tailus/themer";
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    content: ["./*.html", "./*.js", "./*.css"],
+    darkMode: "media",
+    safelist: ["isToggled"],
+    theme: {
+      fontFamily: {
+        sans: ['Geist', 'Inter', ...defaultTheme.fontFamily.sans],
+        mono : ['GeistMono', 'fira-code', ...defaultTheme.fontFamily.mono],
+        pixel : ['Pixelify', 'fira-code', ...defaultTheme.fontFamily.mono],
+      },
+        keyframes: {
+            loop: {
+                to: {
+                    "offset-distance": "100%",
+                },
+            },
+        },
+    },
+    plugins: [
+        themer({
+            palette: {
+                extend : "trust"
+            },
+            radius: "sharp",
+            background: "light",
+            border: "light",
+            padding:"large",
+            shadow : {
+                size : "xl",
+                opacity : "4"
+            },
+            components: {
+                button: {
+                    rounded : "3xl"
+                }
+            }
+        })
+    ],
+};
